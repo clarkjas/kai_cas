b9944fb0607040ee96904d4565d2edaa340fa2ba
Jason Clark <clarkjas@gmail.com>
2024-08-11 14:44:03 +0900

    Initial commit

--- kai/appserver.py
+++ kai/appserver.py
@@ -2,11 +2,11 @@ import flask
 import os
 from cas_bot import application
 
-PORT: int = int(os.environ['SERVER_PORT'])
-HOSTNAME: str = os.environ['SERVER_HOSTNAME']
+SERVER_PORT: int = int(os.environ['SERVER_PORT'])
+SERVER_HOSTNAME: str = os.environ['SERVER_HOSTNAME']
 
 if __name__ == '__main__':
     app = application.flask_app
-    app.run(hostname=HOSTNAME, port=PORT)
+    app.run(host=SERVER_HOSTNAME, port=SERVER_PORT)
 else:
     gunicorn_app = application.flask_app
